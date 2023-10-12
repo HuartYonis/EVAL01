d4345cd3fa56b108b2ee3014dace8420c07f9cc7
Yonis <yonishuart2003@gmail.com>
2023-10-12 11:22:25 +0200

    Version1

---
+++ ClassesFlotteTests/UtilitaireTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassesFlotte;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesFlotte.Tests
+{
+    [TestClass()]
+    public class UtilitaireTests
+    {
+        [TestMethod()]
+        public void ChargeUtileTest()
+        {
+
+            Vehicule veh = new Vehicule("AA-123-BB", "Essence", 8);
+            Utilitaire util = new Utilitaire(15000,3000,50);
+            
+            Assert.AreEqual(12000, util.ChargeUtile());
+        }
+
+        [TestMethod()]
+        public void PrevoirEntretienTest()
+        {
+            // TODO
+        }
+    }
+}
